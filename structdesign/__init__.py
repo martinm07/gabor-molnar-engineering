73e827e1ad48b7828c5d5740b365a20e5e42b260
martinm07 <martin.github07@gmail.com>
2022-07-01 16:30:08 +0100

    Change configuration handling

--- structdesign/__init__.py
+++ structdesign/__init__.py
@@ -4,29 +4,26 @@ from flask import Flask
 
 from .extensions import db
 from .extensions import migrate
+from . import home
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI=r'mysql://root:WeMustKnowWeWillKnow@localhost/mysql_testdb1',
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
-    )
+    app.config.from_prefixed_env()
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.update(test_config)
-    
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from . import home
     app.register_blueprint(home.bp)
 
     return app
